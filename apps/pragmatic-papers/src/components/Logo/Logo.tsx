6f60d744104e75f7dd07e581fcce871bf0af1f16
Patrick Orave <oravep@gmail.com>
2025-06-22 18:33:44 -0500

    Add site web manifest and update Logo component
    - Created a new site.webmanifest file for Progressive Web App support.
    - Updated the Logo component to use a new logo image and adjusted its dimensions.

--- apps/pragmatic-papers/src/components/Logo/Logo.tsx
+++ apps/pragmatic-papers/src/components/Logo/Logo.tsx
@@ -1,3 +1,4 @@
+import { getClientSideURL } from '@/utilities/getURL'
 import clsx from 'clsx'
 import React from 'react'
 
@@ -16,14 +17,14 @@ export const Logo = (props: Props) => {
   return (
     /* eslint-disable @next/next/no-img-element */
     <img
-      alt="Payload Logo"
-      width={193}
-      height={34}
+      alt="Pragmatic Papers Logo"
+      width={420}
+      height={62.64}
       loading={loading}
       fetchPriority={priority}
       decoding="async"
-      className={clsx('max-w-[9.375rem] w-full h-[34px]', className)}
-      src="https://raw.githubusercontent.com/payloadcms/payload/main/packages/ui/src/assets/payload-logo-light.svg"
+      className={clsx('max-w-[26.25rem] w-full h-[34px]', className)}
+      src={getClientSideURL() + '/pragmaticpapers-logo-dark.svg'}
     />
   )
 }
