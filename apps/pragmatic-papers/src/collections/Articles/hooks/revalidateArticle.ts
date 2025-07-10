8612c33b11c5c9676eb4fbe9f6ee6ac9d383a2c3
Ted Timbrell <ted@timbrell.dev>
2025-07-09 19:04:26 -0700

    Fetch relevant volumes to revalidate on article uppdate and delete (#106)
    Closes #97 
    
    - creates a helper to run common validation paths
    - Adds volume query to revalidate related articles
    
    Test plan:
    - Tested publishing and unpublishing articles
    - Tested deleting an article
    
    ---------
    
    Co-authored-by: Patrick Orave <42103411+linkion@users.noreply.github.com>

--- apps/pragmatic-papers/src/collections/Articles/hooks/revalidateArticle.ts
+++ apps/pragmatic-papers/src/collections/Articles/hooks/revalidateArticle.ts
@@ -1,46 +1,62 @@
-import type { CollectionAfterChangeHook, CollectionAfterDeleteHook } from 'payload'
+import type { CollectionAfterChangeHook, CollectionAfterDeleteHook, Payload } from 'payload'
 
 import { revalidatePath, revalidateTag } from 'next/cache'
 
 import type { Article } from '../../../payload-types'
 
-export const revalidateArticle: CollectionAfterChangeHook<Article> = ({
+const revalidateDoc = async (givenDoc: Article, payload: Payload) => {
+  const path = `/articles/${givenDoc.slug}`
+
+  payload.logger.info(`Revalidating article at path: ${path}`)
+  revalidatePath(path)
+  revalidatePath('/feed.articles')
+  revalidateTag('articles-sitemap')
+
+  // Find and revalidate all volumes that reference this article
+  const volumes = await payload.find({
+    collection: 'volumes',
+    where: {
+      'articles.id': {
+        equals: givenDoc.id,
+      },
+    },
+    select: {
+      slug: true,
+    },
+    depth: 0,
+  })
+
+  volumes.docs.forEach((volume) => {
+    const volumePath = `/volumes/${volume.slug}`
+    payload.logger.info(`Revalidating volume at path: ${volumePath}`)
+    revalidatePath(volumePath)
+  })
+}
+
+export const revalidateArticle: CollectionAfterChangeHook<Article> = async ({
   doc,
   previousDoc,
   req: { payload, context },
 }) => {
   if (!context.disableRevalidate) {
     if (doc._status === 'published') {
-      const path = `/articles/${doc.slug}`
-
-      payload.logger.info(`Revalidating article at path: ${path}`)
-
-      revalidatePath(path)
-      revalidatePath('/feed.articles')
-      revalidateTag('articles-sitemap')
+      await revalidateDoc(doc, payload)
     }
 
     // If the article was previously published, we need to revalidate the old path
     if (previousDoc._status === 'published' && doc._status !== 'published') {
-      const oldPath = `/articles/${previousDoc.slug}`
-
-      payload.logger.info(`Revalidating old article at path: ${oldPath}`)
-
-      revalidatePath(oldPath)
-      revalidatePath('/feed.articles')
-      revalidateTag('articles-sitemap')
+      await revalidateDoc(previousDoc, payload)
     }
   }
   return doc
 }
 
-export const revalidateDelete: CollectionAfterDeleteHook<Article> = ({ doc, req: { context } }) => {
+export const revalidateDelete: CollectionAfterDeleteHook<Article> = async ({
+  doc,
+  req: { payload, context },
+}) => {
   if (!context.disableRevalidate) {
-    const path = `/articles/${doc?.slug}`
-
-    revalidatePath(path)
-    revalidatePath('/feed.articles')
-    revalidateTag('articles-sitemap')
+    await revalidateDoc(doc, payload)
   }
 
   return doc
