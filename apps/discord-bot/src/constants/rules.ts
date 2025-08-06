98ec1b37cbe178cf48e39a09ea0489e31627059c
Michael Basala <mbasala@radford.edu>
2025-08-05 21:22:21 -0400

    Fix/rules updates (#164)
    Updated the rules file with the new rules strings. Make minor changes to
    package.json such as author and package name.

--- apps/discord-bot/src/constants/rules.ts
+++ apps/discord-bot/src/constants/rules.ts
@@ -2,32 +2,43 @@ export interface Rule {
   title: string
   description: string
 }
-
 export class Rules {
   public static ServerRules: Rule[] = [
     {
-      title: "Be effective",
-      description: "We are dedicated to electoral outcomes, and have conviction that the work we do matters. You are free to discuss these topics, but will not advocate directly against them."
+      title: "Follow Discord TOS and Guidelines",
+      description: ""
     },
     {
-      title: "Be positive & excellent",
-      description: "Politics is too often a nasty abyss. When people gaze on our work, they ought be blinded by light."
+      title: "Effective Organizing",
+      description: "This server supports electoral efforts, develops strategies, and builds momentum. While critical discussion is welcome, we do not allow direct advocacy against electoral engagement or organizing. If you’re here, it’s because you believe change is possible."
     },
     {
-      title: "Be normal",
-      description: "Don't be this guy: https://www.youtube.com/watch?v=hEiBcZWDnaI"
+      title: "Be Positive",
+      description: "We aim to build a hopeful and constructive space. The goal is for others to be impressed by our discipline, clarity, and energy. Bring your best self."
     },
     {
-      title: "No fed posting",
-      description: "Zero tolerance here. We are a real political org, endorsements of any kind of political violence are **intolerable.**"
+      title: "Zero Tolerance for Fedposting",
+      description: "We are a real political organization. Any mention, endorsement, joke, or casual acceptance of political violence is completely unacceptable. This isn’t r/funny, we all know who Luigi is, don’t try to dance around this rule."
     },
     {
-      title: "Don't meme into the sun",
-      description: "You're responsible for the consequences of your speech. If you want to meme close to the sun, make sure it's really funny."
+      title: "Meme Responsibly",
+      description: "You are welcome to push boundaries and be edgy, but know your audience. If what you say is more mean spirited than it is funny you can get pinged for this rule."
     },
     {
-      title: "Don't be too soy",
-      description: "Don't do anything that would make the average suburban dad vote republican for the rest of their life if they saw it."
+      title: "Cultural Common Sense",
+      description: "Be mindful of how others will perceive your words, actions, or aesthetics. If your message could push an undecided voter toward a reactionary candidate, reconsider what you are about to say. We’re trying to win hearts and minds, not drive people away with self-sabotaging aesthetics."
     },
+    {
+      title: "Do Not Falsely or Poorly Represent the Org",
+      description: "Do not claim to represent DGG Political Action without explicit permission. Whether online or at in-person events, avoid any behavior that could reflect poorly on DGG Political Action or Destiny if you are publicly associated with the group. Violating this rule may result in a ban."
+    },
+    {
+      title: "Respect the Moderators",
+      description:
+        "* If you need assistance from a moderator, please request it by tagging <@&1385638258272108755>  or DMing <@575252669443211264> . Pinging the moderators for no reason (such as meme pinging or ghost pinging) will result in disciplinary actions.\n"+
+        "* Harassing users for reporting rule-breaking is not allowed.\n"+
+        "* No blocking Moderation. They may need to contact you related to the server.\n"+
+        "* If you believe that a moderator has behaved inappropriately, please contact <@764253906736906271>. If you believe <@764253906736906271>  has behaved inappropriately, Contact <@623632405315452929> ."
+    }
   ]
 }
