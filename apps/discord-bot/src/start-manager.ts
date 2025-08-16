b07807ece731d09388ffce62a0098369cb28fe4d
Tyler Malmberg <tkmalmberg@gmail.com>
2025-08-16 15:01:58 -0500

    remove update-server-count-job from start-manager.ts, remove template owner stream and paypal links (#170)
    This function sets the bot's activity to "Streaming to ${Server_count}
    servers"
    
    We also don't plan on putting this bot on any other servers so there's
    no need for this job to run.
    
    closes https://github.com/dggpoliticalaction/org-repo/issues/161

--- apps/discord-bot/src/start-manager.ts
+++ apps/discord-bot/src/start-manager.ts
@@ -3,7 +3,7 @@ import { createRequire } from 'node:module'
 import 'reflect-metadata'
 
 import { GuildsController, RootController, ShardsController } from './controllers/index.js'
-import { type Job, UpdateServerCountJob } from './jobs/index.js'
+import { type Job } from './jobs/index.js'
 import { Api } from './models/api.js'
 import { Manager } from './models/manager.js'
 import { HttpService, JobService, Logger, MasterApiService } from './services/index.js'
@@ -61,7 +61,7 @@ async function start(): Promise<void> {
 
   // Jobs
   const jobs = [
-    Config.clustering.enabled ? undefined : new UpdateServerCountJob(shardManager, httpService),
+    // Config.clustering.enabled ? undefined : new UpdateServerCountJob(shardManager, httpService),
     // TODO: Add new jobs here
   ].filter(Boolean) as Job[]
 
