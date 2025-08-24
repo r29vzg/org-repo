5a1452792edecfa974aaa9fbaa321a72dae4f324
Arthur <r29vzg@gmail.com>
2025-08-24 15:17:24 +0400

    Removed global tailwind config for borderRadius

--- apps/pragmatic-papers/tailwind.config.mjs
+++ apps/pragmatic-papers/tailwind.config.mjs
@@ -52,11 +52,6 @@ const config = {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
       },
-      borderRadius: {
-        lg: 'var(--radius)',
-        md: 'calc(var(--radius) - 2px)',
-        sm: 'calc(var(--radius) - 4px)',
-      },
       colors: {
         /* TODO: Move these definitions to shared styles */
         brand: '#0080ff',
